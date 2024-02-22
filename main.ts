66412baeadef28cfff58fbb4534008b306c672ca
Syeda Sabahat <159911882+sabahat97@users.noreply.github.com>
2024-02-22 21:24:22 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,14 @@
+const data = {
+
+    Servant : "government teacher",
+    Name : " Shakeel uddin",
+    Grade : 19,
+    currentage : 45,
+    RetireYear : 2022,
+    RemainingYearOfRetirement : "5 year"
+
+
+}
+
+console.log(data)
+
